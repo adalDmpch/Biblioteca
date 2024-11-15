c83359603e31510605368a28795882ed07af2c07
adal <adalmendez02@gmail.com>
2024-11-15 12:30:36 -0600

    mi biblioteca

---
+++ resources/views/login.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicia sesión</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}" type="text/css"/>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet">
+
+    @include('layouts.header')
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="offset-md-2 col-lg-5 col-md-7 offset-lg-4 offset-md-3">
+                <div class="panel border bg-white">
+                    <div class="alert-container">
+                        @if(session('status'))
+                            <div class="alert alert-{{ session('status')['type'] }}">
+                                {{ session('status')['message'] }}
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="panel-heading">
+                        <h3 class="pt-3 font-weight-bold">Inicio de sesión</h3>
+                    </div>
+
+                    <div class="panel-body p-3">
+                        <form action="{{ route('login') }}" method="POST">
+                            @csrf
+                            <div class="form-group py-2">
+                                <div class="input-field">
+                                    <span class="far fa-user p-2"></span>
+                                    <input type="email" id="email" name="email" placeholder="Ingresa tu correo electrónico" title="Ingrese un correo válido" autocomplete="off" required>
+                                </div>
+                            </div>
+                            <div class="form-group py-1 pb-2">
+                                <div class="input-field">
+                                    <span class="fas fa-lock px-2"></span>
+                                    <input type="password" id="password" name="password" placeholder="Ingresa tu contraseña" autocomplete="off" required>
+                                </div>
+                            </div>
+                            <div class="form-inline">
+                                <input type="checkbox" name="remember" id="remember">
+                                <label for="remember" class="text-muted">Recordar contraseña</label>
+                                <a href="{{ route('password.request') }}" id="forgot" class="font-weight-bold">¿Recuperar contraseña?</a>
+                            </div>
+                            <button class="btn btn-primary btn-block mt-3" type="submit">Ingresar</button>
+                            <div class="text-center pt-4 text-muted">
+                                ¿No tienes cuenta? <a href="{{ route('register') }}">Crea una cuenta</a>
+                            </div>
+                        </form>
+                    </div>
+
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('layouts.footer')
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+
+// Ruta para mostrar el formulario de recuperación de contraseña
+Route::get('password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+
+// Ruta para enviar el enlace de recuperación de contraseña
+Route::post('password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+
+// Ruta para mostrar el formulario de resetear la contraseña
+Route::get('password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+
+// Ruta para realizar el cambio de contraseña
+Route::post('password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+// Ruta para mostrar el formulario de registro
+Route::get('register', [App\Http\Controllers\Auth\RegisterController::class, 'showRegistrationForm'])->name('register');
+
+// Ruta para procesar el registro de usuario
+Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
